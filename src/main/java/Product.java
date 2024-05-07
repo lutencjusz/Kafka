2e74575a5b320b28a4d4d96894dd3ba74052024d
lutencjusz <lutencjusz@gmail.com>
2024-05-07 14:21:19 +0200

    przeniesienie Kafki z test

---
+++ src/main/java/Product.java
@@ -0,0 +1,14 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
